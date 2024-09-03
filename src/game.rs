449f9205806e746bb52f1897ac422a09debfcaa7
Henri Hosio <henajh@gmail.com>
2024-09-03 15:55:50 +0300

    add canvas edges, add column to invaders, add invader left/right movement, add rand

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
 pub const PIXEL_SIZE: u32 = 6;
 pub const CANVAS_WIDTH: u32 = 1920;
 pub const CANVAS_HEIGHT: u32 = 1080;
+const CANVAS_RIGHT_EDGE: u32 = CANVAS_WIDTH - WIDTH_DIV_20 - 12 * PIXEL_SIZE;
+const CANVAS_LEFT_EDGE: u32 = WIDTH_DIV_20;
 
 const ROW_SIZE: u32 = 11;
 
@@ -8,7 +10,7 @@ const WIDTH_DIV_4: u32 = CANVAS_WIDTH / 4;
 const WIDTH_DIV_20: u32 = CANVAS_WIDTH / 20;
 const WIDTH_DIV_24: u32 = CANVAS_WIDTH / 24;
 const WIDTH_DIV_80: u32 = CANVAS_WIDTH / 80;
-const WIDTH_DIV_160: u32 = CANVAS_WIDTH / 160;
+const WIDTH_DIV_240: u32 = CANVAS_WIDTH / 240;
 const WIDTH_DIV_320: u32 = CANVAS_WIDTH / 320;
 
 const HEIGHT_DIV_4: u32 = CANVAS_HEIGHT / 4;
@@ -85,11 +87,11 @@ impl Player {
     }
 
     pub fn update(&mut self) {
-        if self.moving_left {
+        if self.moving_left && self.game_object.x > CANVAS_LEFT_EDGE {
             self.game_object.x -= 10;
         }
 
-        if self.moving_right {
+        if self.moving_right && self.game_object.x < CANVAS_RIGHT_EDGE {
             self.game_object.x += 10;
         }
 
@@ -111,14 +113,24 @@ impl Player {
 pub struct Invader {
     pub game_object: GameObject,
     pub row: u32,
+    pub column: u32,
     pub dir: String,
 }
 
 impl Invader {
-    pub fn new(x: u32, y: u32, width: u32, height: u32, texture_name: String, row: u32) -> Self {
+    pub fn new(
+        x: u32,
+        y: u32,
+        width: u32,
+        height: u32,
+        texture_name: String,
+        row: u32,
+        column: u32,
+    ) -> Self {
         Invader {
             game_object: GameObject::new(x, y, width, height, texture_name),
             row,
+            column,
             dir: String::from("right"),
         }
     }
@@ -132,7 +144,7 @@ impl Invader {
     }
 
     fn move_down(&mut self) {
-        self.game_object.y -= 1;
+        self.game_object.y += 8 * PIXEL_SIZE;
     }
 }
 
@@ -147,10 +159,10 @@ impl Game {
 
         let mut barrier_row = vec![];
 
-        let mut cur_x = WIDTH_DIV_80;
+        let mut cur_x = WIDTH_DIV_4;
         let mut cur_y = CANVAS_HEIGHT / 6;
 
-        for _i in 0..ROW_SIZE {
+        for i in 0..ROW_SIZE {
             invaders.push(Invader::new(
                 cur_x,
                 cur_y,
@@ -158,15 +170,16 @@ impl Game {
                 8 * PIXEL_SIZE,
                 String::from("invader_texture1"),
                 1,
+                i,
             ));
 
             cur_x += (8 * PIXEL_SIZE) + WIDTH_DIV_80 * 2;
         }
 
-        cur_x = WIDTH_DIV_160 + WIDTH_DIV_320;
+        cur_x = WIDTH_DIV_4;
         cur_y += WIDTH_DIV_20;
 
-        for _i in 0..ROW_SIZE {
+        for i in 0..ROW_SIZE {
             invaders.push(Invader::new(
                 cur_x,
                 cur_y,
@@ -174,15 +187,16 @@ impl Game {
                 8 * PIXEL_SIZE,
                 String::from("invader_texture2"),
                 2,
+                i,
             ));
 
             cur_x += (11 * PIXEL_SIZE) + WIDTH_DIV_80 + WIDTH_DIV_320;
         }
 
-        cur_x = WIDTH_DIV_160 + WIDTH_DIV_320;
+        cur_x = WIDTH_DIV_4 - WIDTH_DIV_240;
         cur_y += WIDTH_DIV_20;
 
-        for _i in 0..ROW_SIZE {
+        for i in 0..ROW_SIZE {
             invaders.push(Invader::new(
                 cur_x,
                 cur_y,
@@ -190,15 +204,16 @@ impl Game {
                 8 * PIXEL_SIZE,
                 String::from("invader_texture2"),
                 3,
+                i,
             ));
 
             cur_x += (11 * PIXEL_SIZE) + WIDTH_DIV_80 + WIDTH_DIV_320;
         }
 
-        cur_x = WIDTH_DIV_160 + WIDTH_DIV_320;
+        cur_x = WIDTH_DIV_4 - WIDTH_DIV_240;
         cur_y += WIDTH_DIV_20;
 
-        for _i in 0..ROW_SIZE {
+        for i in 0..ROW_SIZE {
             invaders.push(Invader::new(
                 cur_x,
                 cur_y,
@@ -206,15 +221,16 @@ impl Game {
                 8 * PIXEL_SIZE,
                 String::from("invader_texture3"),
                 4,
+                i,
             ));
 
             cur_x += (12 * PIXEL_SIZE) + WIDTH_DIV_80;
         }
 
-        cur_x = WIDTH_DIV_160;
+        cur_x = WIDTH_DIV_4 - WIDTH_DIV_240;
         cur_y += WIDTH_DIV_20;
 
-        for _i in 0..ROW_SIZE {
+        for i in 0..ROW_SIZE {
             invaders.push(Invader::new(
                 cur_x,
                 cur_y,
@@ -222,6 +238,7 @@ impl Game {
                 8 * PIXEL_SIZE,
                 String::from("invader_texture3"),
                 5,
+                i,
             ));
 
             cur_x += (12 * PIXEL_SIZE) + WIDTH_DIV_80;
@@ -267,7 +284,10 @@ impl Game {
 
         let mut move_down = false;
         for invader in &invaders_next {
-            if invader.game_object.x == CANVAS_WIDTH {
+            if invader.game_object.x == CANVAS_RIGHT_EDGE {
+                move_down = true;
+                break;
+            } else if invader.game_object.x == CANVAS_LEFT_EDGE {
                 move_down = true;
                 break;
             }
@@ -279,6 +299,8 @@ impl Game {
 
                 if invader.dir == "right" {
                     invader.dir = String::from("left");
+                } else {
+                    invader.dir = String::from("right");
                 }
             }
 
